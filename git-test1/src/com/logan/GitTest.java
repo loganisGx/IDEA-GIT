0fbafb1efb95c5498a7f2de1ff52f8990bddad9d
Logan <137795901@qq.com>
2019-02-28 22:15:20 +0800

    本地IDEA工程添加到git本地版本库

---
+++ git-test1/src/com/logan/GitTest.java
@@ -0,0 +1,11 @@
+package com.logan;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+        System.out.println("hello,world!!!");
+    }
+}
